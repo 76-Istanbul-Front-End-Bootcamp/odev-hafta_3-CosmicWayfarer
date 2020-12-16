7a9603fe4480a458c21870ff2198bab09df4ba60
Aslı Kasap <asli.kasap9@gmail.com>
2020-12-16 19:31:01 +0000

    ödev hazır

--- script.js
+++ script.js
@@ -28,10 +28,7 @@ class Animal {
     petActionTDButton.addEventListener("click",function(event){
        event.stopPropagation();
     });
-    /*for(let i=0;i<petActionTDget.length;i++){
-     petActionTDget[i].addEventListener("click",this.image.bind(this));
-  } 
- */
+  
 }
 action = function(){
   document.getElementById(this.actionSoundName).play();
@@ -40,58 +37,13 @@ action = function(){
 image = function(event){
 let myImg=document.querySelector("img");
 myImg.src=this.srcUrl;
-   /*let bodyEL=document.querySelector("body");
-   let divEL=document.createElement("div");
-   let imgEL=document.createElement("img");
-   imgEL.src=this.srcUrl;
-       event.stopPropagation();
-        bodyEL.appendChild(divEL);
-        divEL.appendChild(imgEL);
-  */
+
   }
 
   
 }
 
-/*
-    // "https://static.vets-now.com/uploads/2017/01/My-cat-had-an-electric-shock-what-should-I-do-Vets-Now.jpg");
-   
 
-
-
-/*
-function Animal(name){
-    this.name = name;
-}
-*/
-/*
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
-  
-}*/
-/*
-Animal.prototype.putInTheDocument = function(){
-    var petsTable = document.getElementById("petsTable");
-    var petTR = document.createElement("tr");
-  
-    var petNameTD = document.createElement("td");
-    petNameTD.textContent = this.name;
-    petTR.appendChild(petNameTD);
-  
-    var petLegsTD = document.createElement("td");
-    petLegsTD.textContent = this.legs;
-    petTR.appendChild(petLegsTD);
-  
-    var petActionTD = document.createElement("td");
-    var petActionTDButton = document.createElement("button");
-    petActionTDButton.textContent = this.actionText;
-    petActionTD.appendChild(petActionTDButton);
-    petTR.appendChild(petActionTD);
-    
-    petActionTDButton.onclick = this.action.bind(this);
-    petsTable.querySelector("tbody").appendChild(petTR)
-}
-*/
 class Cat extends Animal{
 constructor(name){
   super(name); 
@@ -120,26 +72,6 @@ actionSoundName = "scream";
 
 
 }
-/*
-function Cat(name){
-    Animal.call(this, name);
-    this.legs = 4;
-    this.actionText = "Meoow"
-    this.actionSoundName = "meow"
-}
-
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
-    this.legs = 2;
-    this.actionText = "Scream";
-    this.actionSoundName = "scream";
-}
-
-Monkey.prototype = Animal.prototype;
-
-*/
 
 var Mila = new Cat("Mila");
 Mila.putInTheDocument();
